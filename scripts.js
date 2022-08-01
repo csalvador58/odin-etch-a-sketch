e97a1a43688a67474e4384b280fc890e0a4ed09e
Salvador <csalvador58@gmail.com>
2022-07-31 23:50:27 -0700

    Pad sizing feature now available

--- scripts.js
+++ scripts.js
@@ -1,15 +1,16 @@
 const sketchPad = document.querySelector("#sketchPad");
-//const div = document.createElement("div");
-
-const gridSize = 25;
 
 let rowCount = 0;
 let colCount = 0;
+let gridSize = 25; // set default grid size
 
 createNodes(gridSize);
 createSquares(gridSize);
 
+document.querySelector("#gridBtn").addEventListener("click", updateSquares);
+
 function createNodes(a) {
+    removeSquares();
     for (i = 0; i < a; i++) {
         const nodes = document.createElement("div");
         nodes.classList.add(`c${i}`)
@@ -30,6 +31,24 @@ function createSquares(a) {
         }
         count++;
     }
+}
+
+function removeSquares() {
+    const parent = document.querySelector("#sketchPad");
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function updateSquares() {
+    userInput = Number(prompt("Enter new grid size (Required range: 4 - 10):"));
+    while (userInput == NaN || userInput < 4 || userInput > 100) {
+        userInput = Number(prompt("Invalid input, try again (Required range: 4 - 100)."));
+    }
+    gridSize = userInput;
+    createNodes(gridSize);
+    createSquares(gridSize);
+    return;
 }
 
 
